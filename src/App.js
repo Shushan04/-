18b60453099c1e2ce87dfcecba4973205f23a6d0
Shushan04 <ash118817@gmail.com>
2023-07-16 19:40:51 +0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React, { useState } from "react";
+import Header from "./component/header";
+import './app.css';
+
+const App = () =>{
+    return(
+        <div className="container">
+            <div className="app-wrapper">
+                <div>
+                    <Header/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default App; 
 
